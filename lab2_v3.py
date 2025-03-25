0985b5db81ed64bfeefafc973186fd6664c7cd1b
Krzysztof <krzysztof.kotowski2.stud@pw.edu.pl>
2025-03-25 15:20:56 +0100

    feat(ui)!: Made UI with a scaffolded agent

--- lab2_v3.py
+++ lab2_v3.py
@@ -4,6 +4,56 @@ class Nim(object):
 
     # Implement any additional functions needed here
 
+    # -1 -> Error
+    # 0 -> Regular move done
+    # 1 -> No sticks, so current player loses
+    # 2 -> Last stick left, so current player wins
+    def makeAMoveWithEndHandling(self, pile_nr, sticks_amount, isPlayer):
+        if(pile_nr < 0 or pile_nr >= len(game.board)):
+            if(not isPlayer):
+                raise "ERROR! Problem with agent!"
+            print("Pile with number " + str(pile_nr) + " doesn't exist, pick another")
+            return -1
+        if(sticks_amount < 1):
+            if(not isPlayer):
+                raise "ERROR! Problem with agent!"
+            print("You need to take at least one stick (you wanted to take " + str(sticks_amount) + ")!")
+            return -1
+        if(game.board[pile_nr] < sticks_amount):
+            if(not isPlayer):
+                raise "ERROR! Problem with agent!"
+            print("You can take at most " + str(game.board[pile_nr]) + " sticks from pile " + str(pile_nr) + ", while you wanted to take " + str(sticks_amount) + "!")
+            return -1
+        
+        if not isPlayer:
+            print("Agent's move: pile chosen = " + str(pile_nr) + ", sticks taken =" + str(sticks_amount))
+        self.board[pile_nr] -= sticks_amount
+        if(sum(self.board) == 0):
+            if(isPlayer):
+                print("You lose")
+            else:
+                print("You win")
+            return 1
+        elif(sum(self.board) == 1):
+            if(isPlayer):
+                print("You win")
+            else:
+                print("You lose")
+            return 1
+        return 0
+    
+    def agentDecision(self):
+        """
+        TODO
+        Temporary functionality to test ui
+        """
+        pile_nr, sticks_amount = 0, 1
+        for i in range(len(self.board)):
+            if(self.board[i] > 0):
+                pile_nr = i
+                break
+        return pile_nr, sticks_amount
+
     def minimax(self, board, depth, maximizing_player, alpha, beta):
         """
         Minimax with alpha-beta pruning algorithm
@@ -45,6 +95,38 @@ if __name__ == "__main__":
     print("The person who removes the last stick loses!")
     print("Example: to remove from 2nd pile 3 sticks , enter 2 3")
 
-    while True:
-       print("Pile state %s" % (game.board))
-       # Your code starts here
+    hasGameEnded = False
+    isPlayersTurn = True
+    while hasGameEnded is False:
+        print("Pile state %s" % (game.board))
+
+        # Your code starts here
+        """
+        Process:
+        1. Get input from user
+        2. Apply the move to the board
+        3. If zero sticks - current player loses, if one, current player wins
+        4. Make agent do its turn
+        5. Apply the move to the board
+        6. [3]
+        """
+        while(True):
+            pile_nr, sticks_amount = None, None
+
+            if(isPlayersTurn):
+                user_input = input("Your move (<pile_nr> <sticks_amount>): ")
+                a = [int(x) for x in user_input.split()]
+                pile_nr, sticks_amount = a[0], a[1]
+            else:
+                ## Agent's turn
+                pile_nr, sticks_amount = game.agentDecision()
+
+            move_result = game.makeAMoveWithEndHandling(pile_nr, sticks_amount, isPlayersTurn)
+
+            if(move_result == -1):
+                continue
+            elif(move_result == 1):
+                hasGameEnded = True
+            break
+
+        isPlayersTurn = not isPlayersTurn
