335bd1836426e6f57b2a17253ffaac4190a4037d
Krzysztof <krzysztof.kotowski2.stud@pw.edu.pl>
2025-04-02 20:10:08 +0200

    feat(minmax)!: Implemented minmax with alpha-beta pruning

--- lab2_v3.py
+++ lab2_v3.py
@@ -1,6 +1,14 @@
+import copy
+
 class Nim(object):
     def __init__(self, board):
         self.board = board
+
+    # Nim constants
+    NIM_INF = 123456
+    NIM_EMPTY_MOVE = (-1, -1)
+    NIM_WIN_VALUE = 1
+    NIM_LOSS_VALUE = -1
 
     # Implement any additional functions needed here
 
@@ -8,7 +16,7 @@ class Nim(object):
     # 0 -> Regular move done
     # 1 -> No sticks, so current player loses
     # 2 -> Last stick left, so current player wins
-    def makeAMoveWithEndHandling(self, pile_nr, sticks_amount, isPlayer):
+    def makeAMoveWithEndHandling(self, pile_nr, sticks_amount, isPlayer) -> int:
         if(pile_nr < 0 or pile_nr >= len(game.board)):
             if(not isPlayer):
                 raise "ERROR! Problem with agent!"
@@ -34,27 +42,34 @@ class Nim(object):
             else:
                 print("You win")
             return 1
-        elif(sum(self.board) == 1):
-            if(isPlayer):
-                print("You win")
-            else:
-                print("You lose")
-            return 1
+        #elif(sum(self.board) == 1):
+        #    if(isPlayer):
+        #        print("You win")
+        #    else:
+        #        print("You lose")
+        #    return 1
         return 0
     
     def agentDecision(self):
         """
         TODO
         Temporary functionality to test ui
-        """
+        
         pile_nr, sticks_amount = 0, 1
         for i in range(len(self.board)):
             if(self.board[i] > 0):
                 pile_nr = i
                 break
         return pile_nr, sticks_amount
+        """
+        ##_, next_move = self.simpleminmax(self.board, sum(self.board), True)
+        ##return next_move
+        _, next_move = self.minimax(self.board, sum(self.board), True, -abs(self.NIM_INF), abs(self.NIM_INF))
+        return next_move
 
-    def minimax(self, board, depth, maximizing_player, alpha, beta):
+
+    def minimax(self, board : list[int], depth : int, maximizing_player : bool,
+                alpha : int, beta : int) -> tuple[int, tuple[int, int]]:
         """
         Minimax with alpha-beta pruning algorithm
 
@@ -70,6 +85,70 @@ class Nim(object):
 
         """
         # Your code starts here
+        if(depth == 0 or sum(board) == 0):
+            if(maximizing_player):
+                return self.NIM_WIN_VALUE, self.NIM_EMPTY_MOVE
+            else:
+                return -1, self.NIM_EMPTY_MOVE
+
+        if(maximizing_player):
+            value = -abs(Nim.NIM_INF)
+            for iter_pile_index in range(0, len(board)):
+                for iter_sticks_taken_from_pile in range(1, board[iter_pile_index]+1):
+                    tempBoard = copy.deepcopy(board)
+                    tempBoard[iter_pile_index] = tempBoard[iter_pile_index] - iter_sticks_taken_from_pile
+                    new_value, _ = self.simpleminmax(tempBoard, depth-1, False)
+                    if(new_value > alpha):
+                        alpha = new_value
+                        best_move = (iter_pile_index, iter_sticks_taken_from_pile)
+                    if(beta <= alpha):
+                        break
+        else:
+            value = abs(Nim.NIM_INF)
+            for iter_pile_index in range(0, len(board)):
+                for iter_sticks_taken_from_pile in range(1, board[iter_pile_index]+1):
+                    tempBoard = copy.deepcopy(board)
+                    tempBoard[iter_pile_index] = tempBoard[iter_pile_index] - iter_sticks_taken_from_pile
+                    new_value, _ = self.simpleminmax(tempBoard, depth-1, True)
+                    if(new_value < beta):
+                        beta = new_value
+                        best_move = (iter_pile_index, iter_sticks_taken_from_pile)
+                    if(beta <= alpha):
+                        break
+        return value, best_move
+        
+    def simpleminmax(
+            self, board : list[int], depth : int,
+            maximizing_player : bool) -> tuple[int, tuple[int, int]]:
+        if(depth == 0 or sum(board) == 0):
+            if(maximizing_player):
+                return self.NIM_WIN_VALUE, self.NIM_EMPTY_MOVE
+            else:
+                return -1, self.NIM_EMPTY_MOVE
+
+        if(maximizing_player):
+            best_value = -abs(Nim.NIM_INF)
+            for iter_pile_index in range(0, len(board)):
+                for iter_sticks_taken_from_pile in range(1, board[iter_pile_index]+1):
+                    tempBoard = copy.deepcopy(board)
+                    tempBoard[iter_pile_index] = tempBoard[iter_pile_index] - iter_sticks_taken_from_pile
+                    new_value, _ = self.simpleminmax(tempBoard, depth-1, False)
+                    if(new_value > best_value):
+                        best_value = new_value
+                        best_move = (iter_pile_index, iter_sticks_taken_from_pile)
+        else:
+            best_value = abs(Nim.NIM_INF)
+            for iter_pile_index in range(0, len(board)):
+                for iter_sticks_taken_from_pile in range(1, board[iter_pile_index]+1):
+                    tempBoard = copy.deepcopy(board)
+                    tempBoard[iter_pile_index] = tempBoard[iter_pile_index] - iter_sticks_taken_from_pile
+                    new_value, _ = self.simpleminmax(tempBoard, depth-1, True)
+                    if(new_value < best_value):
+                        best_value = new_value
+                        best_move = (iter_pile_index, iter_sticks_taken_from_pile)
+
+        return best_value, best_move
+
 
 if __name__ == "__main__":
 
@@ -116,7 +195,7 @@ if __name__ == "__main__":
             if(isPlayersTurn):
                 user_input = input("Your move (<pile_nr> <sticks_amount>): ")
                 a = [int(x) for x in user_input.split()]
-                pile_nr, sticks_amount = a[0], a[1]
+                pile_nr, sticks_amount = a[0]-1, a[1]
             else:
                 ## Agent's turn
                 pile_nr, sticks_amount = game.agentDecision()
